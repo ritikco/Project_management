4f6c94b4a426c3639c89211af3ed20de7cca0a63
ritikco <ritikTyagi1312@gmail.com>
2025-07-17 13:30:39 +0530

    next commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 const pathRoutes = require('./path');
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 
-app.use(express.json()); 
 
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -17,7 +20,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
 
 app.use('/api', pathRoutes);
 
